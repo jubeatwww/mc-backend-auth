5c26e8906617d3df29c9937757a45c70738a95d4
jubeatWwW <l6104400@gmail.com>
2019-07-10 16:21:00 +0800

    feat: 🎸 Add header
    Add a test user header for test

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Header } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('/auth')
+  @Header('user_info', 'test')
   getHello(): string {
     return this.appService.getHello();
   }
